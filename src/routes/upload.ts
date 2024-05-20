30625b9608b7208f4ffb3a6cbb877da4bd737624
nnlgsakib <nlgarts@outlook.com>
2024-05-20 15:33:02 +0600

    Added globle uploader and L3 encryption

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -2,55 +2,31 @@ import express, { Request, Response } from 'express';
 import multer from 'multer';
 import { generateEthereumStyleHash } from '../utils/hash';
 import { db } from '../utils/db';
-
-// Import encryption functions
 import { encryptData, keccak256 } from '../utils/encryption';
 
 const uploadRouter = express.Router();
 const upload = multer();
 
-uploadRouter.post('/image', upload.single('image'), async (req: Request, res: Response) => {
+// Single endpoint to handle all file uploads
+uploadRouter.post('/', upload.single('file'), async (req: Request, res: Response) => {
     try {
         if (!req.file) {
             return res.status(400).json({ message: 'No file uploaded' });
         }
 
-        const { buffer: imageBuffer, mimetype, originalname } = req.file;
-        const hash = generateEthereumStyleHash(imageBuffer.toString('base64'));
+        const { buffer, mimetype, originalname } = req.file;
+        const hash = generateEthereumStyleHash(Math.floor(new Date().getTime() / 1000).toString());
         const encryptedHash = keccak256(hash);
 
         // Encrypt the data and metadata
-        const encryptedData = encryptData(imageBuffer.toString('base64'), encryptedHash);
+        const encryptedData = encryptData(buffer.toString('base64'), encryptedHash);
         const metadata = JSON.stringify({ mimetype, originalname, data: encryptedData });
         const encryptedMetadata = encryptData(metadata, encryptedHash);
 
         await db.put(encryptedHash, encryptedMetadata);
         return res.status(200).json({ hash });
     } catch (error) {
-        console.error('Error uploading image:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-});
-
-uploadRouter.post('/video', upload.single('video'), async (req: Request, res: Response) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ message: 'No file uploaded' });
-        }
-
-        const { buffer: videoBuffer, mimetype, originalname } = req.file;
-        const hash = generateEthereumStyleHash(videoBuffer.toString('base64'));
-        const encryptedHash = keccak256(hash);
-
-        // Encrypt the data and metadata
-        const encryptedData = encryptData(videoBuffer.toString('base64'), encryptedHash);
-        const metadata = JSON.stringify({ mimetype, originalname, data: encryptedData });
-        const encryptedMetadata = encryptData(metadata, encryptedHash);
-
-        await db.put(encryptedHash, encryptedMetadata);
-        return res.status(200).json({ hash });
-    } catch (error) {
-        console.error('Error uploading video:', error);
+        console.error('Error uploading file:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 });
